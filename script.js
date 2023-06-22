aa963d08ac3f977fa588c4d5b091feeb9bf81ef5
Andrew Morrison <morrison.andrew422@gmail.com>
2023-06-22 10:49:51 -0400

    update portfolio

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 // cashed element references
+var projects = document.querySelectorAll(".hover-action");
 // constants
 // app's state (variables)
 // event listeners
+projects.forEach(function (project) {
+    var sibling = null;
+    var hideTimeout = null;
+    project.addEventListener('mouseover', function (event) {
+        console.dir(event.target);
+        var targetElement = event.target;
+        sibling = targetElement.nextElementSibling;
+        if (sibling && sibling.style.display !== 'flex') {
+            sibling.style.display = 'flex';
+        }
+        if (hideTimeout) {
+            clearTimeout(hideTimeout);
+        }
+    });
+    project.addEventListener('mouseleave', function (event) {
+        console.dir(event.target);
+        if (sibling && sibling.style.display === 'flex') {
+            hideTimeout = window.setTimeout(function () {
+                if (sibling) {
+                    sibling.style.display = 'none';
+                }
+            }, 500); // Adjust the delay as needed
+        }
+    });
+});
 // functions
--- script.ts
+++ script.ts
@@ -1,9 +1,40 @@
 // cashed element references
+const projects = document.querySelectorAll<HTMLDivElement>(".hover-action");
 
 // constants
 
 // app's state (variables)
 
 // event listeners
+projects.forEach((project: HTMLDivElement) => {
+  let sibling: HTMLDivElement | null = null;
+  let hideTimeout: number | null = null;
+
+  project.addEventListener('mouseover', (event: MouseEvent) => {
+    console.dir(event.target);
+    const targetElement = event.target as HTMLDivElement;
+    sibling = targetElement.nextElementSibling as HTMLDivElement;
+    if (sibling && sibling.style.display !== 'flex') {
+      sibling.style.display = 'flex';
+    }
+    if (hideTimeout) {
+      clearTimeout(hideTimeout);
+    }
+  });
+
+  project.addEventListener('mouseleave', (event: MouseEvent) => {
+    console.dir(event.target);
+    if (sibling && sibling.style.display === 'flex') {
+      hideTimeout = window.setTimeout(() => {
+        if (sibling) {
+          sibling.style.display = 'none';
+        }
+      }, 500); // Adjust the delay as needed
+    }
+  });
+});
+
+
+
 
 // functions
